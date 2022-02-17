6793252108d1f9082aee2319db89581369fa55d4
Milexy zambrano <milexyzambrano.a@gmail.com>
2022-02-16 22:28:51 -0500

    tarea

---
+++ tarea.js
@@ -0,0 +1,14 @@
+//TAREA EJERCICIOS
+//verificar si una de las cadena de caracteres del array tiene mas de 10 caracteres
+
+const vectorcad=["Ecuador","Argentina","Dinamarca","Corea del Norte"];
+
+
+
+//Chequear si uno de los elementos de la matriz es true
+
+const vectorBooleano=[[false,false,false],[false,false,false],[false,false,true]];
+
+//Verificar si la palabra "Lost" existe dentro del siguiente array
+
+const vectorParrafo=["Strange is the night black stars rise","And strange moons circle through the skies","But stranger still is","Lost Carcacosa"];
