dd29d8e00c1769bd1d57ce068c676e259de2122e
Your Name <you@example.com>
2023-01-10 17:40:53 -0500

    tarea

---
+++ tareaPF.js
@@ -0,0 +1,9 @@
+function palabras(str) {
+    var vocales = ["a","e","i","o","u","A","E","I","O","U"]
+    return str.split("").filter (data => {
+        return vocales.indexOf(data.toLowerCase())!=-1;
+    }).join('');
+}
+const p1=palabras("Esmeraldas");
+console.log(p1); 
+
